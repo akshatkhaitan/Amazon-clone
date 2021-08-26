692ad9bef7f8495d2e80ca066880b96674f16e24
Akshat Khaitan <khaitanakshat40@gmail.com>
2021-08-26 20:42:08 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./Login";
+import { useEffect } from "react";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
+import Checkout from "./Checkout"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [user, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
+	return (
+		<div className="App">
+			<Router>
+				<Header />
+				<Switch>
+        <Route path="/checkout">
+						<Checkout />
+					</Route>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDp09M06WWhC30FHsSfSHjK3IMrvCNlOBk",
+  authDomain: "challenge-be63b.firebaseapp.com",
+  projectId: "challenge-be63b",
+  storageBucket: "challenge-be63b.appspot.com",
+  messagingSenderId: "566159496466",
+  appId: "1:566159496466:web:9feb99b299149f22f70fcb",
+  measurementId: "G-PJ281K369D"
+};
+
+  
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
